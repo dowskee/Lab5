3dda2f73d2dd4083037509d09861852c96f2fef5
dowskee <31831031+dowskee@users.noreply.github.com>
2017-09-28 14:32:53 -0400

    Made loop

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -10,53 +10,67 @@ namespace Lab5
     {
         static void Main(string[] args)
         {
-
-            int DieSide1, DieSide2;
-
-            DieSide1 = ReadInteger();
-
-            DieSide2 = ReadInteger();
-
-            // application asks user to enter number of sides for die
-            // then application prompts user to roll die
-            // application will roll two (n-sided) die, and display the result of each
-            // application will ask if user wants to roll again 
-
-            int Result = Sum(DieSide1, DieSide2); //sum returned as result
-            Console.WriteLine(Result); //write result for user 
-
-        }
-
-        public static int ReadInteger()
-        {
-            int Input;
-            Console.WriteLine("Welcome to the Grand Circus Casino! I'm your host, Grant Chirpus! Please select any number-sided die of your choosing:");
-            Input = int.Parse(Console.ReadLine());
-
-            return Input; //takes number selected and produces random generation of two numbers (Die1 and Die2) for next method
-
-        }
-
-        public static int Sum(int Die1, int Die2)
-        {
-            int Result = Die1 + Die2;
-
-            if (DieSide1 + DieSide2 == 2) 
+            bool RollAgain = true;
+            while (RollAgain == true)
             {
-                Console.WriteLine("Snake Eyes!");
+
+                int DieSide1, DieSide2;
+
+                DieSide1 = ReadInteger();
+
+                DieSide2 = ReadInteger();
+
+                // application asks user to enter number of sides for die
+                // then application prompts user to roll die
+                // application will roll two (n-sided) die, and display the result of each
+                // application will ask if user wants to roll again 
+
+                int Result = Sum(DieSide1, DieSide2); //sum returned as result
+                Console.WriteLine(Result); //write result for user 
+
             }
 
-            else if (DieSide1 + DieSide2 == 7 || DieSide1 + DieSide2 ==11)
+            public static int ReadInteger()
             {
-                Console.WriteLine("Craps!");
+                int SideSelect;
+                Console.WriteLine("Welcome to the Grand Circus Casino! I'm your host, Grant Chirpus! Please select any number-sided die of your choosing:");
+                SideSelect = int.Parse(Console.ReadLine());
+
+                return SideSelect; //takes number selected and produces random generation of two numbers (Die1 and Die2) for next method
+
             }
 
-            else if (DieSide1 + DieSide2 ==12)
+            public static int Sum(int Die1, int Die2)
             {
-                Console.WriteLine("Box Cars!");
-            }
-            return Result;
+                int Result = Die1 + Die2;
 
+                if (DieSide1 + DieSide2 == 2) //sum of 1+1 is 2, snake eyes
+                {
+                   Console.WriteLine("Snake Eyes!");
+                }
+
+                else if (DieSide1 + DieSide2 == 7 || DieSide1 + DieSide2 == 11)
+                {
+                   Console.WriteLine("Craps!"); //craps is total of 7 or 11
+                }
+
+                else if (DieSide1 == 6 && DieSide2 == 6)
+                {
+                   Console.WriteLine("Box Cars!"); //box cars returns two 6-faced die. 
+                }
+                    return Result;
+                }
+            
+
+            string UserSelection;
+            Console.WriteLine("Roll Again? (Y or N?)");
+            UserSelection = Console.ReadLine();
+            
+            if ((UserSelection == "n") || (UserSelection == "N") || (UserSelection == "no") || (UserSelection == "No"))
+            {
+            RollAgain = false;
+            Console.WriteLine("Your loss. House wins!");
+            }
         }
     }
 }
